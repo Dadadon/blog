9663268003c231d219eb794310bbda6e9fa90335
dadadon <davianberoni@yahoo.com>
2023-03-03 11:49:34 -0500

    Update models

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,7 @@ class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   belongs_to :post, counter_cache: true
 
+  validates :text, presence: true
   # def self.update_post_comments_counter(comment)
   #   post = comment.post
   #   post.update(comments_counter: post.comments.counter)
@@ -9,4 +10,5 @@ class Comment < ApplicationRecord
   def update_post_comments_counter(_comment)
     post.increment!(:comments_counter)
   end
+  
 end
