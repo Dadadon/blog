eccb6de4215f80bfe783dde796a79375cce7a18a
dadadon <davianberoni@yahoo.com>
2023-03-10 12:36:28 -0500

    Generate controller for post

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,53 @@
+class PostsController < ApplicationController
+    before_action :set_post, only: [:show, :edit, :update, :destroy]
+  
+    def index
+      @user = User.find(params[:user_id])
+      @posts = @user.posts
+    end
+  
+    def show
+      @post = Post.find(params[:id])
+    end
+  
+    def new
+      @post = Post.new
+    end
+  
+    def create
+      @post = current_user.posts.build(post_params)
+  
+      if @post.save
+        redirect_to @post, notice: "Post was successfully created."
+      else
+        render :new
+      end
+    end
+  
+    def edit
+    end
+  
+    def update
+      if @post.update(post_params)
+        redirect_to @post, notice: "Post was successfully updated."
+      else
+        render :edit
+      end
+    end
+  
+    def destroy
+      @post.destroy
+      redirect_to posts_url, notice: "Post was successfully destroyed."
+    end
+  
+    private
+  
+    def set_post
+      @post = Post.find(params[:id])
+    end
+  
+    def post_params
+      params.require(:post).permit(:title, :text)
+    end
+  end
+  
