d7b1e600f3210562c8f38aaa563e5e286da59601
dadadon <davianberoni@yahoo.com>
2023-03-10 12:48:02 -0500

    Fix linter errors

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,36 +1,36 @@
 class CommentsController < ApplicationController
-    before_action :set_post, only: [:create, :destroy]
-  
-    def create
-      @comment = @post.comments.build(comment_params)
-      @comment.author = current_user
-  
-      if @comment.save
-        @comment.update_post_comments_counter(@comment)
-        redirect_to @post, notice: 'Comment was successfully created.'
-      else
-        render 'posts/show'
-      end
+  before_action :set_post, only: %i[create destroy]
+
+  def create
+    @comment = @post.comments.build(comment_params)
+    @comment.author = current_user
+
+    if @comment.save
+      @comment.update_post_comments_counter(@comment)
+      redirect_to @post, notice: 'Comment was successfully created.'
+    else
+      render 'posts/show'
     end
-  
-    def destroy
-      @comment = Comment.find(params[:id])
-  
-      if @comment.destroy
-        @comment.update_post_comments_counter(@comment)
-        redirect_to @post, notice: 'Comment was successfully destroyed.'
-      else
-        redirect_to @post, alert: 'Failed to destroy comment.'
-      end
+  end
+
+  def destroy
+    @comment = Comment.find(params[:id])
+
+    if @comment.destroy
+      @comment.update_post_comments_counter(@comment)
+      redirect_to @post, notice: 'Comment was successfully destroyed.'
+    else
+      redirect_to @post, alert: 'Failed to destroy comment.'
     end
-  
-    private
-  
-    def set_post
-      @post = Post.find(params[:post_id])
-    end
-  
-    def comment_params
-      params.require(:comment).permit(:text)
-    end
-  end  
+  end
+
+  private
+
+  def set_post
+    @post = Post.find(params[:post_id])
+  end
+
+  def comment_params
+    params.require(:comment).permit(:text)
+  end
+end
