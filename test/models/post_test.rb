448864fc41983525ac3bad8f4d7d46149bde663d
dadadon <davianberoni@yahoo.com>
2023-04-06 14:26:26 -0500

    Add test for post model

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,36 +1,29 @@
 require 'test_helper'
-
 class PostTest < ActiveSupport::TestCase
   def setup
-    @user = User.create(name: 'John Robin', posts_counter: 0)
-    @post = Post.new(title: 'Test Title', text: 'Test Text', comments_counter: 0, likes_counter: 0, author: @user)
+    @user = users(:one)
+    @post = Post.new(title: "Test Post", text: "Test post body", author: @user)
   end
 
-  test 'post should be valid' do
-    assert @post.valid?
-  end
 
-  test 'title should be present' do
-    @post.title = '     '
+  test "title should be present" do
+    @post.title = ""
     assert_not @post.valid?
   end
 
-  test 'title should not be too long' do
-    @post.title = 'a' * 251
+  test "title should not be too long" do
+    @post.title = "a" * 251
     assert_not @post.valid?
   end
 
-  test 'comments counter should be an integer greater than or equal to zero' do
+  test "comments_counter should be greater than or equal to 0" do
     @post.comments_counter = -1
     assert_not @post.valid?
-    @post.comments_counter = 0
-    assert @post.valid?
   end
 
-  test 'likes counter should be an integer greater than or equal to zero' do
+  test "likes_counter should be greater than or equal to 0" do
     @post.likes_counter = -1
     assert_not @post.valid?
-    @post.likes_counter = 0
-    assert @post.valid?
   end
+
 end
