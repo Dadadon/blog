457570869fbf961ad4d1c2089a21acec0d3901db
dadadon <davianberoni@yahoo.com>
2023-04-06 14:26:34 -0500

    Add test for user model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,17 +1,21 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  test 'Users name must not be blank' do
-    user = User.new
-    assert_not user.save, 'Saved the user without a name'
+  setup do
+    @user = users(:one)
   end
 
-  test 'posts counter must be an integer greater than or equal to zero' do
-    user = User.new(name: 'John')
-    user.posts_counter = 10
-    assert user.valid?, 'Valid user with valid posts_counter'
+  test 'should be valid' do
+    assert @user.valid?
+  end
 
-    user.posts_counter = -1
-    assert_not user.valid?, 'Invalid user with negative posts_counter'
+  test 'name should be present' do
+    @user.name = '  '
+    assert_not @user.valid?
+  end
+
+  test 'posts_counter should not be negative' do
+    @user.posts_counter = -1
+    assert_not @user.valid?
   end
 end
