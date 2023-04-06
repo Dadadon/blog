c0acd14aa3ee75000fab94840045d40bb2d0c742
dadadon <davianberoni@yahoo.com>
2023-04-06 14:24:53 -0500

    Update file

--- app/models/post.rb
+++ app/models/post.rb
@@ -14,13 +14,12 @@ class Post < ApplicationRecord
 
   after_save :update_post_counter
 
-  # private
 
-  # def update_post_counter
-  #   author.increment!(:posts_counter)
-  # end
+  def update_post_counter
+    author.increment!(:posts_counter)
+  end
 
-  # def recent_comments(num = 5)
-  #   comments.order(created_at: :desc).limit(num)
-  # end
+  def recent_comments(num = 5)
+    comments.order(created_at: :desc).limit(num)
+  end
 end
