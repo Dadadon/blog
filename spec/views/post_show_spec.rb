6304f894d852bb49c98022947316f5efeb363f59
Juanpa8830 <jp3_4@hotmail.com>
2023-04-21 11:07:52 -0500

    Refactor test for postst title and post author

--- spec/views/post_show_spec.rb
+++ spec/views/post_show_spec.rb
@@ -8,17 +8,17 @@ RSpec.describe 'post_show', type: :feature do
       bio: 'User1 bio',
       posts_counter: 0
     )
-    @post1 = Post.create(author: @user1, title: 'post', text: 'post', comments_counter: 0, likes_counter: 0)
+    @post1 = Post.create(author: @user1, title: 'post1', text: 'post', comments_counter: 0, likes_counter: 0)
     @comment1 = Comment.create(author: @user1, post: @post1, text: 'comments')
     visit user_post_path(@user1, @post1)
   end
 
   it 'I can see the posts title' do
-    expect(page).to have_content(@post1.title)
+    expect(page).to have_content('post1')
   end
 
   it 'I can see who wrote the post' do
-    expect(page).to have_content(@post1.author.name)
+    expect(page).to have_content('User 1')
   end
 
   it 'I can see how many comments it has.' do
